bb0d3dbbf839bedc9af1f221f8da35c0eb5476d5
rdas <rana.das@hrblock.com>
2017-05-23 10:55:49 +0100

    first commint after adding placeholders

---
+++ src/main/java/com/rdas/bootone/BootoneApplication.java
@@ -0,0 +1,12 @@
+package com.rdas.bootone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootoneApplication.class, args);
+	}
+}
---
+++ src/test/java/com/rdas/bootone/BootoneApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rdas.bootone;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BootoneApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
